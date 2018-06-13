ac66a725c759c412447681d135d790ad95829e7e
Adrey Smirnov <iadisey@gmail.com>
2018-06-13 15:46:13 +0300

    Commit changes

--- js/my_promice-3.js
+++ js/my_promice-3.js
@@ -2,25 +2,34 @@ function applayForViva(document, resolve, reject) {
     console.log('Запускаем получение визы....');
     let promiseVisa = new Promise(function (resolve, reject) {
         setTimeout(function () {
-            Math.random() > .3 ? resolve({}) : reject('В визе отказано');
+            Math.random() > .5 ? resolve({}) : reject('В визе отказано');
         }, 1000);
     });
     return promiseVisa;
 }
 
-function bookHotel() {
+function getVisa(visa) {
+    console.info('Виза получена!');
+    return Promise.resolve('UK');
 }
 
-function buyTikets() {
+function bookHotel(visa) {
+    console.log(`Виза`,visa);
+    console.log(`Бронируем Отель`);
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => resolve(`Зарезервировано`),1000);
+    });
+}
+
+function buyTikets(boking) {
+    console.log(`Бронь`, boking);
+    console.log(`Покупаем билет`);
 }
 
 applayForViva({})
-    .then(
-        function (visa) {
-            console.info('Виза получена!');
-        },
-        function (reason) {
-            console.error(reason);
-
-        });
+    .then(getVisa)
+    .then(bookHotel)
+    .then(buyTikets)
+    .catch(error => console.error(error))
+    .then(() => console.info(`Процедура завершена`));
 
