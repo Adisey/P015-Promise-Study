775b89217cc9d4db72934e22a22dcc1c64a213c1
Adrey Smirnov <iadisey@gmail.com>
2018-06-13 18:48:28 +0300

    111

--- js/my_promise-4.js
+++ js/my_promise-4.js
@@ -2,8 +2,28 @@ function go(num) {
     return new Promise(function (resolve, reject) {
         let delay = Math.ceil(Math.random() * 3000);
         console.log(num, delay);
-        setTimeout(() => resolve(num), delay);
+        setTimeout(() => {
+            if (delay > 2000)
+                reject(num);
+            else
+                resolve(num);
+        }, delay);
     });
 
 }
 
+let p1 = go(1);
+let p2 = go(2);
+let p3 = go(3);
+
+Promise.all([p1, p2, p3])
+    .then(value => console.log(`Promise -`, value))
+    .catch(error => console.error('Error P - ', error));
+console.log(`---`);
+let p6 = go(6);
+let p7 = go(7);
+let p8 = go(8);
+
+Promise.race([p6, p7, p8])
+    .then(value => console.log(`First -`, value))
+    .catch(error => console.error(' E - ', error));
